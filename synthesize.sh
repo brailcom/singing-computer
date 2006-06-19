23411a7a04265ac32c1880a864aacbf56500679f
pdm <pdm>
2006-06-19 19:14:13 +0000

    Fri Jun 16 20:41:08 CEST 2006  pdm@brailcom.org   * synthesize.sh script written

---
+++ synthesize.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+usage () {
+  echo "usage: $0 FILE.xml [ LANGUAGE-CODE-OR-VOICE [ SPEEDUP ] ]"
+  echo "       $0 FILE.ly [ LANGUAGE-CODE-OR-VOICE ]"
+  exit 1
+}
+
+process_xml () {
+  local file="$1"
+  local voice="$2"
+  local speedup="$3"
+  local wave="${file%xml}wav"
+  local tmpwave="$wave.tmp.$$"
+
+  trap "rm -f '$tmpwave'" EXIT
+  text2wave -eval "($voice)" -mode singing "$file" -o "$tmpwave"
+  if [ -n "$speedup" ] && [ $speedup -ne 1 ]; then
+    sox "$tmpwave" "$wave" speed $speedup
+  else
+    mv "$tmpwave" "$wave"
+  fi
+  echo "$wave created."
+}
+
+process_ly () {
+  local lyfile="$1"
+  local voice="$2"
+
+  lilypond "$lyfile"
+  local xmlfile=$(ls -t $(dirname "$lyfile") | grep '\.xml$' | head -1)
+  if [ -z "$xmlfile" ]; then
+    echo "No XML file found."
+    exit 1
+  fi
+
+  speedup=$(awk '/^#\(set! song:\*base-octave-shift\* -?[0-9]+\)/ { shift=$3 } END { print exp(-shift) }' "$lyfile")
+  process_xml "$xmlfile" "$voice" $speedup
+}
+
+file="$1"
+voice="$2"
+speedup="$3"
+
+if [ $# -lt 1 ] || [ $# -gt 3 ]; then
+  usage
+fi
+
+if [ -z "$voice" ] || [ "$voice" = "en" ]; then
+  voice=voice_kal_diphone
+elif [ "$voice" = "cs" ]; then
+  voice=voice_czech_ph
+fi
+
+if [ "$file" = "${file%ly}" ]; then
+  process_xml "$file" "$voice" "$speedup"
+else
+  if [ -n "$speedup" ]; then
+    usage
+  fi
+  process_ly "$file" "$voice"
+fi
+
+exit 0
