85402f3f5062177fe07bd3fee049527229f0e350
pdm <pdm>
2006-07-20 17:22:14 +0000

    Thu Jul 20 19:21:14 CEST 2006  pdm@brailcom.org   * Emacs: Support for playing midi files together with singing

--- lilypond-festival.el
+++ lilypond-festival.el
@@ -122,7 +122,7 @@ only."
                                             'LilyPond-song-list-history)))
         (setq LilyPond-default-songs (nreverse song-list))))))
 
-(defun LilyPond-xml->wav (filename)
+(defun LilyPond-file->wav (filename)
   (format "%s.wav" (file-name-sans-extension filename)))
 
 (defun LilyPond-song-update-needed (source results)
@@ -144,21 +144,25 @@ only."
     (LilyPond-change-language)))
 
 (defun LilyPond-play-song (song)
-  (call-process LilyPond-play-command nil 0 nil (LilyPond-xml->wav song)))
+  (call-process LilyPond-play-command nil 0 nil (LilyPond-file->wav song)))
 
-(defun LilyPond-sing-list (songs &optional parallel)
+(defun LilyPond-sing-list (songs &optional parallel midi-files)
   (LilyPond-update-language)
   (let ((commands nil)
         (update-needed (or (LilyPond-song-update-needed
                             (buffer-file-name) songs)
                            (LilyPond-song-update-needed
-                            (LilyPond-get-master-file) songs))))
+                            (LilyPond-get-master-file) songs)
+                           (LilyPond-song-update-needed
+                            (buffer-file-name) midi-files)
+                           (LilyPond-song-update-needed
+                            (LilyPond-get-master-file) midi-files))))
     (when update-needed
       (push (format "%s %s" LilyPond-lilypond-command
                     (LilyPond-get-master-file))
             commands))
     (mapc #'(lambda (song)
-              (let ((wav-file (LilyPond-xml->wav song)))
+              (let ((wav-file (LilyPond-file->wav song)))
                 (when (or update-needed
                           (file-newer-than-file-p song wav-file)
                           (not (equal LilyPond-language
@@ -169,8 +173,16 @@ only."
                                 (or LilyPond-language ""))
                         commands))))
           songs)
-    (let ((wav-files (mapcar 'LilyPond-xml->wav songs)))
-      (if (eq parallel 'mix)
+    (mapc #'(lambda (midi)
+              (let ((wav-file (LilyPond-file->wav midi)))
+                (when (or update-needed
+                          (file-newer-than-file-p midi wav-file))
+                  (push (format "%s -Ow -o %s %s"
+                                LilyPond-midi-command wav-file midi)
+                        commands))))
+          midi-files)
+    (let ((wav-files (mapcar 'LilyPond-file->wav (append songs midi-files))))
+      (if (and (eq parallel 'mix) (not midi-files) (cdr wav-files))
           (push (format "%s %s -t wav - | %s -t wav -"
                         LilyPond-mix-command
                         (mapconcat 'identity wav-files " ")
@@ -203,26 +215,56 @@ only."
         (LilyPond-sing-list songs)
       (error "No song found in the current buffer"))))
 
-(defun LilyPond-command-sing-parallel ()
-  "Play selected songs in parallel."
+(defun LilyPond-command-sing-parallel (&optional midi-files)
+  "Play selected songs in parallel.
+The optional MIDI-FILES argument is a list of midi file names to play together
+with singing."
   (interactive)
   (let ((songs (LilyPond-song-list)))
     (if songs
-        (LilyPond-sing-list songs (if (cdr songs) 'mix nil))
+        (LilyPond-sing-list songs 'mix midi-files)
       (error "No songs given"))))
+
+(defun LilyPond-command-sing-play-current-midi ()
+  "Sing selected songs together with playing current midi."
+  (interactive)
+  (LilyPond-command-sing-parallel (list (LilyPond-string-current-midi))))
+
+(defun LilyPond-command-sing-play-all-midi ()
+  "Sing all songs together with playing all midi."
+  (interactive)
+  (let ((midi-string (LilyPond-string-all-midi))
+        (midi-files '()))
+    (while (string-match "^\\([^ ]+\\) \\(.*\\)$" midi-string)
+      (push (match-string 1 midi-string) midi-files)
+      (setq midi-string (match-string 2 midi-string)))
+    (let ((songs (LilyPond-all-songs)))
+      (if songs
+          (LilyPond-sing-list songs 'mix (nreverse midi-files))
+        (error "No songs found")))))
 
 (defun LilyPond-command-sing (&optional all)
   "Sing song arround the current point.
-If invoked with a prefix argument, sing all songs of the current buffer."
+If invoked with a universal argument, sing all songs of the current buffer.
+If invoked with a zero prefix argument, sing selected songs in parallel.
+If invoked with a negative prefix argument, sing selected songs together with
+playing current midi file.
+If invoked with a positive prefix argument, sing all songs together with
+playing all midi files."
   (interactive "P")
   (cond
    ((not all)
     (LilyPond-command-sing-current))
    ((listp all)
     (LilyPond-command-sing-all))
-   (t
-    (LilyPond-command-sing-parallel))))
-
+   ((numberp all)
+    (cond
+     ((= all 0)
+      (LilyPond-command-sing-parallel))
+     ((< all 0)
+      (LilyPond-command-sing-play-current-midi))
+     ((> all 0)
+      (LilyPond-command-sing-play-all-midi))))))
 
 (define-key LilyPond-mode-map "\C-c\C-a" 'LilyPond-command-sing)
 
@@ -232,6 +274,10 @@ If invoked with a prefix argument, sing all songs of the current buffer."
   ["Sing Parallel" LilyPond-command-sing-parallel t])
 (easy-menu-add-item LilyPond-command-menu nil
   ["Sing All" LilyPond-command-sing-all t])
+(easy-menu-add-item LilyPond-command-menu nil
+  ["Sing and Play" LilyPond-command-sing-play-current-midi t])
+(easy-menu-add-item LilyPond-command-menu nil
+  ["Sing and Play All" LilyPond-command-sing-play-all-midi t])
 
 
 ;;; Announce
